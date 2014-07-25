c78bad9b0ee029ea2fba08fd451e6afee91d1d36
Your Name <do.vincent@live.com>
2014-07-25 01:59:45 -0400

    Remove middlewares

--- web.js
+++ web.js
@@ -3,10 +3,6 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.bodyParser());
-app.use(express.logger("default"));
-app.use(express.cookieParser());
-
  
 // app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
