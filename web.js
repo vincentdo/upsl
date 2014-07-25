66f2e353526e54cd35b263d0dc8ed151843915df
Your Name <do.vincent@live.com>
2014-07-25 01:58:27 -0400

    Remove routes

--- web.js
+++ web.js
@@ -1,14 +1,12 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var path = require('path');
-var routes = require('./routes/routes.js');
 var app = express();
 
 app.use(express.bodyParser());
 app.use(express.logger("default"));
 app.use(express.cookieParser());
 
-app.post('/addPlayer', routes.add_player);
  
 // app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
